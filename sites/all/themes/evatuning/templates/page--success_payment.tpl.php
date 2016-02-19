31f8f1d67f62039e9f0c658d5a1bb2d3f06844d4
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-02-19 21:04:11 +0200

    shift

--- sites/all/themes/evatuning/templates/page--success_payment.tpl.php
+++ sites/all/themes/evatuning/templates/page--success_payment.tpl.php
@@ -4,44 +4,29 @@
  * @link http://www.mohamedelsayed.net
  * @copyright Copyright (c) 2016 Programming by "http://www.mohamedelsayed.net"
  */?>
-<?php $base_url_with_lang = elsayed_get_base_url_with_lang();
-$actual_link = elsayed_get_actual_link();?>
-<?php include_once 'header.php';?>
-<div id="products-post">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="product-heading">
-                    <h2><?php echo t('Checkout with Paypal');?></h2>
-                    <img src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme();?>/images/under-heading.png" alt="" >
+<?php 
+if(isset($_POST['txn_id'])){
+    $item_transaction = $_POST['txn_id'];// Paypal transaction ID
+    ellabad_save_order($item_transaction);
+    $base_url_with_lang = elsayed_get_base_url_with_lang();
+    $actual_link = elsayed_get_actual_link();?>
+    <?php include_once 'header.php';?>
+    <div id="products-post">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="product-heading">
+                        <h2><?php echo t('Checkout with Paypal');?></h2>
+                        <img src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme();?>/images/under-heading.png" alt="" >
+                    </div>
                 </div>
             </div>
+            <div style="text-align: center;color: #cb0202;">
+                <h1>Payment Successful, We will contact you as soon as possible</h1>
+            </div>           
         </div>
-        <div>
-            <?php
-            $item_no            = $_REQUEST['item_number'];
-            $item_transaction   = $_REQUEST['tx']; // Paypal transaction ID
-            $item_price         = $_REQUEST['amt']; // Paypal received amount
-            $item_currency      = $_REQUEST['cc']; // Paypal received currency type
-            ellabad_save_order($item_transaction);
-            $price = '10.00';
-            $currency='USD';
-
-            //Rechecking the product price and currency details
-            if($item_price==$price && $item_currency==$currency)
-            {
-                $content = "<h1>Welcome, Guest</h1>";
-                $content .= "<h1>Payment Successful</h1>";
-            }
-            else
-            {
-                $content = "<h1>Payment Failed</h1>";
-            }
-
-            $title = "PayPal Payment in PHP";
-            $heading = "Welcome to PayPal Payment PHP example.";
-            ?>
-        </div>                
     </div>
-</div>
-<?php include_once 'footer.php';?>
+    <?php include_once 'footer.php';?>
+<?php }else {
+    drupal_access_denied();
+}?>
